f19dce500dbcfb0b66d5e1a5c678e788a69c5a04
Niranjan BV <59027578+NiranjanBV@users.noreply.github.com>
2021-02-08 21:18:30 +0530

    1st-commit

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,1 +1,16 @@
 //WAP to find the distance between two points using structures and 4 functions.
+// This code is incomplete because i do not have enough knowledge about structures 
+#include <stdio.h>
+#include<sdlib.h>
+#include<math.h>
+
+struct coordinates ;
+{
+int x1;
+int y1;
+int x2;
+int y2;
+}
+int main()
+{
+int x1
