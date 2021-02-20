28badd575394b9ef08c83f008ddca8ad6def1509
Niranjan BV <59027578+NiranjanBV@users.noreply.github.com>
2021-02-20 17:00:59 +0530

    updated

--- distance.c
+++ distance.c
@@ -1,10 +1,10 @@
 //WAP to find the distance between two point using 4 functions.
 #include<stdio.h>
 #include <math.h>
-float input(char i,int j)
+float input(char *i,int *j)
 {
     float u;
-    printf("Enter the value of %c%d coordinate: ",i,j);
+    printf("Enter the value of %c%d coordinate: ",*i,*j);
     scanf("%f",&u);
     return u;
 }
@@ -20,11 +20,13 @@ void output(float x1,float x2,float y1,float y2,float distance)
 }
 int main()
 {
+    char a = 'x',b = 'y' ;
+    int c = 1,d = 2;
     float x1,y1,x2,y2,res;
-    x1=input('x',1);
-    y1=input('y',1);
-    x2=input('x',2);
-    y2=input('y',2);
+    x1=input(&a,&c);
+    y1=input(&b,&c);
+    x2=input(&a,&d);
+    y2=input(&b,&d);
     res=distance(x2,x1,y2,y1);
     output(x1,x2,y1,y2,res);
     return 0;
