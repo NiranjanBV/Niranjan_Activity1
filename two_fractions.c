7df4fcd9e47d32c1990ec461c6c4408086c5fca6
Niranjan BV <59027578+NiranjanBV@users.noreply.github.com>
2021-02-21 10:56:27 +0530

    updated

--- two_fractions.c
+++ two_fractions.c
@@ -1,21 +1,44 @@
 //WAP to find the sum of two fractions.
 #include <stdio.h>
-struct input 
+#include <math.h>
+struct fraction
 {
-float first_fraction , second_fraction;
+    int x1, y1, x2, y2;
 };
-float answer(struct input S1)
+void addition(struct fraction f)
 {
-float sum;
-sum = S1.first_fraction + S1.second_fraction;
-return sum;
+    int num, denom, i, gcd, x, y;
+    num = (f.x1 * f.y2) + (f.x2 * f.y1);
+    denom = (f.y1 * f.y2);
+    for(i=1; i<=num && i<=denom; ++i)
+    {
+        if(num % i == 0 && denom % i == 0)
+        {
+            gcd = i;
+        }
+    }
+    x = num/gcd;
+    y = denom/gcd;
+    if(y==1)
+        printf("The fraction sum is: %d/%d = %d", x, y, x);
+    else
+        printf("The fraction sum is: %d/%d", num/gcd, denom/gcd);
 }
-float main()
+void input()
 {
-struct input S1;
-printf("enter the value of first fraction :");
-scanf("%f",&S1.first_fraction);
-printf("enter the value of second fraction :");
-scanf("%f",&S1.second_fraction);
-printf("the sum of %f and %f is %f ",S1.first_fraction,S1.second_fraction , answer(S1));
+    struct fraction f1;
+    printf("Enter the numerator for 1st number: ");
+    scanf("%d", &f1.x1);
+    printf("Enter the denominator for 1st number: ");
+    scanf("%d", &f1.y1);
+    printf("Enter the numerator for 2nd number: ");
+    scanf("%d", &f1.x2);
+    printf("Enter the denominator for 2nd number: ");
+    scanf("%d", &f1.y2);
+    addition(f1);
+}
+int main()
+{
+    input();
+    return 0;
 }
